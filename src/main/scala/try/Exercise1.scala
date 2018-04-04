e13799a425ec9a154951b058e880a74db8c43e11
Jeremie Jost <jeremiejost@gmail.com>
2018-04-04 23:05:01 +0100

    Changes

--- src/main/scala/try/Exercise1.scala
+++ src/main/scala/try/Exercise1.scala
@@ -1,23 +1,15 @@
 package `try`
 
+
 import scala.io.StdIn
-import scala.util.{Failure, Success, Try}
-
-object CatAgeCalculator {
-
-  def calculateCatAge(input: String, ageFactor: Int): Try[Int] = {
-    // 1. Convert the input to an int (this can fail!)
-    // 2. Divide the input by ageFactor
-    ???
-  }
-}
 
 object Exercise1 extends App {
   val factor = 7
-  val humanAge = StdIn.readLine("Enter your age :")
+  val input = StdIn.readLine("Enter your age :")
 
-  CatAgeCalculator.calculateCatAge(humanAge, factor) match {
-    case Success(age) => println("You are $age cat years old.")
-    case Failure(_) => println("Enter a valid value please.")
+  val humanAge = try {
+    input.toInt
   }
+  val catAge = humanAge / 7
+  println(s"Your are $catAge cat years old!")
 }
