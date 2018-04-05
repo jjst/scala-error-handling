16a7b71708c85745e6a3063436d64050c43115c2
Jeremie Jost <jeremie.jost@zalando.ie>
2018-04-05 16:00:30 +0100

    All the changes

--- src/main/scala/try/Exercise1.scala
+++ src/main/scala/try/Exercise1.scala
@@ -5,11 +5,10 @@ import scala.io.StdIn
 
 object Exercise1 extends App {
   val factor = 7
-  val input = StdIn.readLine("Enter your age :")
+  val defaultAge = 30
+  val input = StdIn.readLine(s"Enter your age [$defaultAge]:")
 
-  val humanAge = try {
-    input.toInt
-  }
+  val humanAge = input.toInt
   val catAge = humanAge / 7
   println(s"Your are $catAge cat years old!")
 }
--- src/main/scala/try/Exercise2.scala
+++ src/main/scala/try/Exercise2.scala
@@ -3,19 +3,24 @@ package `try`
 import scala.io.StdIn
 import scala.util.{Failure, Success, Try}
 
-object GenericAnimalAgeCalculator {
+object AgeCalculatorService {
 
-  def calculateAge(input: String, ageFactor: String): Try[Int] = {
-    ???
+  def calculateAge(humanAge: Int, ageMultiplier: Int = 7): Int = {
+    require(humanAge >= 0, "Invalid human age! A human can't have a negative age, silly!")
+    require(ageMultiplier > 0, "Invalid age multiplier, it has to be positive")
+    humanAge / ageMultiplier
   }
 }
 
 object Exercise2 extends App {
   val humanAge = StdIn.readLine("Enter your age :")
-  val factor = StdIn.readLine("Enter the human-to-animal ratio:")
+  val ageMultiplier = StdIn.readLine("Enter the human-to-animal ratio:")
 
-  GenericAnimalAgeCalculator.calculateAge(humanAge, factor) match {
-    case Success(age) => println("You are $age cat years old.")
-    case Failure(_) => println("Enter a valid value please.")
-  }
+  // Use Try and a for-comprehension to do the following:
+  // - parse the humanAge to an Int
+  // - parse the ageMultiplier to an Int
+  // - call calculateAge
+  // If the result is a success, print it on standard out
+  // Otherwise, print the message from the corresponding exception to stderr.
+  AgeCalculatorService.calculateAge(-1, -1)
 }
